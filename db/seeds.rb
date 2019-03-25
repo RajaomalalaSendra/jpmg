5d8bd267015754314b3d833fa6e80245e75eb43c
RajaomalalaSendra <rajaomalalasendrampanjato@gmail.com>
2019-03-25 12:07:28 +0300

    modification of the seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,14 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-# japanese = ['こにちは','ありがとうございます','日本','みんな','ごめんなさい','大丈夫']
-# romanji = ['KONICHIWA','ARIGATO GOZAIMASU', 'NIHON', 'MINNA', 'GOMENNASAI', 'DAIJOBU']
-# malagasy = ['manahoana', 'misaotra betsaka', 'Japana', 'ny rehetra', 'miala tsiny', 'tsara']
-# 6.times do |i|
-# 	Word.create(japanese: japanese[i], romanji: romanji[i], malagasy: malagasy[i])
-# end
-require 'csv'
-words = CSV.read("fra.csv")
-50.times do |i|
+japanese = ['こにちは','ありがとうございます','日本','みんな','ごめんなさい','大丈夫']
+romanji = ['KONICHIWA','ARIGATO GOZAIMASU', 'NIHON', 'MINNA', 'GOMENNASAI', 'DAIJOBU']
+malagasy = ['manahoana', 'misaotra betsaka', 'Japana', 'ny rehetra', 'miala tsiny', 'tsara']
+6.times do |i|
+	Word.create(japanese: japanese[i], romanji: romanji[i], malagasy: malagasy[i])
+end
+words = [['Go.', 'Hi.', 'Run!', 'Run!', 'Run!', 'Wow!', 'Fire!', 'Help!', 'Jump.', 'Stop!', 'Stop!'],['Va !','Salut !', 'Cours !', 'Courez !', 'Ça alors !', 'Au feu !', 'À l\'aide !','Saute.', 'Ça suffit !', 'Stop !']]
+11.times do |i|
   Sentence.create(english: words[i][0], french: words[i][1])
 end
